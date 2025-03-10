ce8d4ce725b2d6c29ee4c3080275fe62b93be0ec
Rohit Singh <smartyonpc@gmail.com>
2025-03-11 00:04:18 +0530

    made new changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,8 +8,7 @@ function ProductList({ searchQuery }) {
     );
 
     return (
-            <div className="grid grid-cols-3 justify-between gap-20 p-6 ml-5">
-
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-4 md:gap-6 lg:gap-8 p-6 ml-5">
 
             {filteredProducts.length > 0 ? (
                 filteredProducts.map((product) => (
